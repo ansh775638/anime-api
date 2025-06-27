2b2112ff839d6f735789cdcedcfc7c89b8365aa8
ansh775638 <thebaapansh@gmail.com>
2025-06-27 12:51:03 +0530

    Update episodeList.controller.js

--- src/controllers/episodeList.controller.js
+++ src/controllers/episodeList.controller.js
@@ -1,21 +1,50 @@
 import extractEpisodesList from "../extractors/episodeList.extractor.js";
 import { getCachedData, setCachedData } from "../helper/cache.helper.js";
+import { getInternalIdFromAnilistId, isAnilistId } from "../helper/anilistMapper.helper.js";
 
-export const getEpisodes = async (req,res) => {
+export const getEpisodes = async (req, res) => {
   const { id } = req.params;
   // const cacheKey = `episodes_${id}`;
+  
   try {
+    let internalId = id;
+    
+    // Check if this is an AniList ID (numeric only)
+    if (isAnilistId(id)) {
+      // Convert AniList ID to internal ID
+      const mappedId = await getInternalIdFromAnilistId(id);
+      
+      if (!mappedId) {
+        return { 
+          success: false, 
+          message: `No anime found with AniList ID: ${id}` 
+        };
+      }
+      
+      internalId = mappedId;
+    }
+    
     // const cachedResponse = await getCachedData(cacheKey);
     // if (cachedResponse && Object.keys(cachedResponse).length > 0) {
     //   return cachedResponse;
     // }
-    const data = await extractEpisodesList(encodeURIComponent(id));
+    
+    const data = await extractEpisodesList(encodeURIComponent(internalId));
+    
     // setCachedData(cacheKey, data).catch((err) => {
     //   console.error("Failed to set cache:", err);
     // });
-    return data;
+    
+    // Format the response to include a success field
+    return {
+      success: true,
+      results: data
+    };
   } catch (e) {
     console.error("Error fetching episodes:", e);
-    return e;
+    return {
+      success: false,
+      message: e.message || "An error occurred while fetching episodes"
+    };
   }
 };
